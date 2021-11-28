bc60d0227108f0a343fa42cd01c4aec1455053c7
Abdumutal Abdusamatov <mebungler@gmail.com>
2021-11-28 15:00:10 +0500

    initial setup

---
+++ screens/Home.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import {SafeAreaView, ScrollView, View} from 'react-native';
+import Categories from '../components/Categories';
+import HeaderTabs from '../components/HeaderTabs';
+import Header from '../components/HeaderTabs';
+import RestaurantItems from '../components/RestaurantItems';
+import SearchBar from '../components/SearchBar';
+
+export default function Home() {
+  return (
+    <SafeAreaView style={{backgroundColor: '#eee', flex: 1}}>
+      <View style={{backgroundColor: 'white', padding: 15}}>
+        <HeaderTabs />
+        <SearchBar />
+      </View>
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <Categories />
+        <RestaurantItems />
+      </ScrollView>
+    </SafeAreaView>
+  );
+}
